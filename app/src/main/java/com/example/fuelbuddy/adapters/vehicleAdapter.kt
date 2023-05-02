fa031c6076314f1288e10059ca7a438b285cf7a5
it21268694 <99591042+it21268694@users.noreply.github.com>
2023-05-02 14:44:22 +0530

    complet Vehicle Adapter

---
+++ app/src/main/java/com/example/fuelbuddy/adapters/vehicleAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.fuelbuddy.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fuelbuddy.R
+import com.example.fuelbuddy.dataClasses.vehicleList
+
+class vehicleAdapter(private val vehicleList : ArrayList<vehicleList>) :
+    RecyclerView.Adapter<vehicleAdapter.vehicleViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): vehicleViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.vehicle_items, parent, false)
+        return vehicleViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return vehicleList.size
+    }
+
+    override fun onBindViewHolder(holder: vehicleViewHolder, position: Int) {
+
+        val currentPost = vehicleList[position]
+
+        holder.vehicleName.text = currentPost.vehiType
+        holder.vehicleNumber.text = currentPost.vehinum
+
+    }
+
+    class vehicleViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val vehicleName : TextView = view.findViewById(R.id.vehiName)
+        val vehicleNumber : TextView = view.findViewById(R.id.vehicleNumber)
+
+    }
+}
