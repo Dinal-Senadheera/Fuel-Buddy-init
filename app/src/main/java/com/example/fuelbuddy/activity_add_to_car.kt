b5fce9afa45862a1a0e6ca29e3c985dcf87e08e2
it21268694 <99591042+it21268694@users.noreply.github.com>
2023-05-04 01:13:09 +0530

    663

--- app/src/main/java/com/example/fuelbuddy/activity_add_to_car.kt
+++ app/src/main/java/com/example/fuelbuddy/activity_add_to_car.kt
@@ -6,8 +6,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.example.fuelbuddy.dataClasses.vehicleList
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class activity_add_to_car : AppCompatActivity() {
     private lateinit var vehicleNumber: TextInputLayout
@@ -17,20 +22,24 @@ class activity_add_to_car : AppCompatActivity() {
     private lateinit var vehiShu: Button
     private lateinit var userName: TextView
     private lateinit var auth: FirebaseAuth
+    private lateinit var database : DatabaseReference
+    private var uid: String? =null
     private var name: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_to_car)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("vehicle")
         val user = auth.currentUser
 
         user?.let {
             name = it.displayName
+            uid = it.uid
         }
 
-        vehicleNumber = findViewById(R.id.vehicleNumber)
-        VehicleType = findViewById(R.id.VehicleType)
+        vehicleNumber = findViewById(R.id.vehinum)
+        VehicleType = findViewById(R.id.vehiType)
         chassisNumber = findViewById(R.id.chassisNumber)
         puleType = findViewById(R.id.puleType)
         vehiShu = findViewById(R.id.vehiShu)
@@ -38,5 +47,19 @@ class activity_add_to_car : AppCompatActivity() {
         userName.text = "$name"
 
 
+
+
+    }
+    private fun savePosts() {
+
+        val uniqueID: String = UUID.randomUUID().toString()
+        val userID: String = auth.currentUser?.uid.toString()
+        val vehicleList = vehicleList(userID ,vehinum , vehiType , chassisNumber,puleType)
+
+        database.child(uniqueID).setValue(vehicleList).addOnCompleteListener{
+            Toast.makeText(this, "Post added Successfully", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this,"Error" , Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/fuelbuddy/dataClasses/vehicleList.kt
+++ app/src/main/java/com/example/fuelbuddy/dataClasses/vehicleList.kt
@@ -1,6 +1,9 @@
 package com.example.fuelbuddy.dataClasses
 
 data class vehicleList(
+    var userID: String? = null,
+    var vehinum :String ?= null,
     var vehiType : String ?= null,
-    var vehinum :String ?= null
+    var chassisNumber :String?=null,
+    var puleType : String? = null
 )
