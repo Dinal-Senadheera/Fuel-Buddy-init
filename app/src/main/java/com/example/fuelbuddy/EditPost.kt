ec9eb85d8bfb76f46eee7643b3de3d87fb25b693
ShenanPerera <shenaperera2001@gmail.com>
2023-05-03 22:44:53 +0530

    "modify xml"

--- app/src/main/java/com/example/fuelbuddy/EditPost.kt
+++ app/src/main/java/com/example/fuelbuddy/EditPost.kt
@@ -2,16 +2,23 @@ package com.example.fuelbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class EditPost : AppCompatActivity() {
+//    private lateinit var fuelType : EditText
+//    private lateinit var qty : EditText
+//    private lateinit var uPrice : EditText
+//    private lateinit var btnUpdate : Button
+//    private lateinit var btnDelete : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.update_delete)
 
 //        val type : EditText = findViewById(R.id.fuelType)
-//        val qty : EditText = findViewById(R.id.qtyInput)
+//        val fqty : EditText = findViewById(R.id.qtyInput)
 //        val price : EditText = findViewById(R.id.edt_unitPrice)
 //
 //        val update : Bundle ?= intent.extras
