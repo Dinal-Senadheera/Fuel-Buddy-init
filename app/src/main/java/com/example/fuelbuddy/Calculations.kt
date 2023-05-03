2560c56ca1ae41d42651ec9ff3234a0f8853b7b9
Dinal-Senadheera <dinalsenadheera@gmail.com>
2023-05-03 17:42:29 +0530

    "Bug fixes"

---
+++ app/src/main/java/com/example/fuelbuddy/Calculations.kt
@@ -0,0 +1,10 @@
+package com.example.fuelbuddy
+
+class Calculations(
+    private val number1: Int,
+    private val number2: Int,
+    private val number3: Int
+) {
+    fun doubleMultiplication() = number1.toDouble() * doubleDivision()
+    fun doubleDivision() = number2.toDouble() / number3
+}
