ed5fbf86854b0483b77aed7c31791d196defa967
ShenanPerera <shenaperera2001@gmail.com>
2023-05-06 12:43:50 +0530

    "Unit Testing"

--- app/src/test/java/com/example/fuelbuddy/PostUnitTest.kt
+++ app/src/test/java/com/example/fuelbuddy/PostUnitTest.kt
@@ -1,2 +1,23 @@
 package com.example.fuelbuddy
 
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class PostUnitTest{
+    val profit = calculateProfit(10 , 50 , 1)
+
+    @Test
+    fun calProfit_isCorrect(){
+        assertEquals(500.0, profit.calProfit())
+    }
+
+    @Test
+    fun calCommission_isCorrect(){
+        assertEquals(5.0, profit.calCommssion())
+    }
+
+    @Test
+    fun calTotalProfit_isCorrect(){
+        assertEquals(495.0 , profit.totalProfit())
+    }
+}
