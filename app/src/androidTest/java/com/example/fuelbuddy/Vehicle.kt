afbc23e573596119fc2874ed25f7937a32ad7fb0
it21268694 <99591042+it21268694@users.noreply.github.com>
2023-05-06 19:43:55 +0530

    test input

--- app/src/androidTest/java/com/example/fuelbuddy/Vehicle.kt
+++ app/src/androidTest/java/com/example/fuelbuddy/Vehicle.kt
@@ -1,51 +1,53 @@
 package com.example.fuelbuddy
 
 import android.content.Context
-import android.widget.EditText
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.material.textfield.TextInputLayout
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.regex.Pattern.matches
 
-class PostInstumentedTest {
+class Vehicle {
     private lateinit var appContext : Context
 
     @Test
     fun useAppContext(){
         //Context of the app under test
+
         appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.fuelbuddy", appContext.packageName)
+        assertEquals("com.example.fuelbuddy",appContext.packageName)
     }
 
     @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(AddFuel::class.java)
-    private lateinit var activityScenario : ActivityScenario<AddFuel>
+    val activityScenarioRule = ActivityScenarioRule(activity_add_to_car::class.java)
+    private lateinit var activityScenario: ActivityScenario<activity_add_to_car>
 
     @Before
     fun setUp(){
         activityScenario = activityScenarioRule.scenario
-        activityScenario.onActivity { activity ->
+        activityScenario.onActivity{ activity ->
+            activity.findViewById<TextInputLayout>(R.id.vehinum).editText?.setText("")
+            activity.findViewById<TextInputLayout>(R.id.vehiType).editText?.setText("")
+            activity.findViewById<TextInputLayout>(R.id.chassisNumber).editText?.setText("")
+            activity.findViewById<TextInputLayout>(R.id.puleType).editText?.setText("")
 
-            activity.findViewById<EditText>(R.id.fuelType).setText("")
-            activity.findViewById<EditText>(R.id.qtyInput).setText("")
-            activity.findViewById<EditText>(R.id.edt_unitPrice).setText("")
+
         }
-
-
-        }
+    }
 
     @Test
     fun testForm(){
-        onView(withId(R.id.btnSubmit)).perform(click())
-        onView(withId(R.id.add_fuel)).check(matches(withText(R.string.page_title)))
+        onView(withId(R.id.vehiShu)).perform(click())
+        onView(withId(R.id.Add_vehiclesTopic)).check(matches(withText(R.string.Add_vehiclesTopic)))
     }
+
 
 }
